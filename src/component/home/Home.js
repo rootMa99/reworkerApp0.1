c5bdb84d6b0d175a2fc5f0ce33b4fd48cc822598
anass zeroual <anasszeroual09@gmail.com>
2024-02-15 17:07:43 +0100

    pushinki

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -146,71 +146,145 @@ const Home = (p) => {
   };
 
   return (
-    <div className={c.holder}>
-      {(popUp || popUpEdite.states) && <BackDrop click={clickHandler} />}
-      {popUp && <PopupFormRef click={clickHandler} page={page.page} />}
-      {popUpEdite.states && (
-        <PopupCmAdSl data={popUpEdite.data} click={clickHandler} />
-      )}
+    <React.Fragment>
+      {isLoged.role !== "Logistics" && (
+        <div className={c.holder}>
+          {(popUp || popUpEdite.states) && <BackDrop click={clickHandler} />}
+          {popUp && <PopupFormRef click={clickHandler} page={page.page} />}
+          {popUpEdite.states && (
+            <PopupCmAdSl data={popUpEdite.data} click={clickHandler} />
+          )}
 
-      {isLoged.role === "Reworker" && (
-        <button className={c.button} onClick={clickHandler}>
-          scan reference
-        </button>
-      )}
-      <h2 className={c.titleCab}>urgent cables</h2>
-      <table
-        className={`${c.table} ${c.tableur}`}
-        style={{ marginBottom: "2rem" }}
-      >
-        <thead>
-          <tr style={{ backgroundColor: "black" }}>
-            <th width="auto">date/time</th>
-            <th width="auto">Ref</th>
-            <th>Equipe</th>
-            <th width="30%">Problem</th>
-            <th width="15%">Details</th>
-            <th>pdd*</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {urgentData.length > 0 &&
-            urgentData.map((m) => (
-              <tr key={m._id}>
-                <td>
-                  {m.createdAt.split("T")[0]}
-                  <br />
-                  <span className={c.timed}>
-                    {m.createdAt.split("T")[1].split(":")[0]}:
-                    {m.createdAt.split("T")[1].split(":")[1]}
-                  </span>
-                </td>
-                <td>{m.reference}</td>
-                <td>{m.crew}</td>
-                <td style={{ padding: "8px 0" }}>
-                  <ul className={c.underl}>
-                    {m.problem.map((m, i) => (
-                      <li key={i} className={c.listu}>
-                        {m}
-                      </li>
-                    ))}
-                  </ul>
-                </td>
-                <td>{m.details}</td>
-                <td>{m.pDD}</td>
-                <td style={stylec(m.cableStatus)}>{m.cableStatus}</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-      {isLoged.role === "ShiftLeader" && (
-        <React.Fragment>
-          <h2 className={c.titleCabsl}>scrap cables</h2>
+          {isLoged.role === "Reworker" && (
+            <button className={c.button} onClick={clickHandler}>
+              scan reference
+            </button>
+          )}
+          <h2 className={c.titleCab}>urgent cables</h2>
           <table
-            className={`${c.table} ${c.tableurscrap}`}
-            // style={{ marginBottom: "2rem" }}
+            className={`${c.table} ${c.tableur}`}
+            style={{ marginBottom: "2rem" }}
           >
+            <thead>
+              <tr style={{ backgroundColor: "black" }}>
+                <th width="auto">date/time</th>
+                <th width="auto">Ref</th>
+                <th>Equipe</th>
+                <th width="30%">Problem</th>
+                <th width="15%">Details</th>
+                <th>pdd*</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {urgentData.length > 0 &&
+                urgentData.map((m) => (
+                  <tr key={m._id}>
+                    <td>
+                      {m.createdAt.split("T")[0]}
+                      <br />
+                      <span className={c.timed}>
+                        {m.createdAt.split("T")[1].split(":")[0]}:
+                        {m.createdAt.split("T")[1].split(":")[1]}
+                      </span>
+                    </td>
+                    <td>{m.reference}</td>
+                    <td>{m.crew}</td>
+                    <td style={{ padding: "8px 0" }}>
+                      <ul className={c.underl}>
+                        {m.problem.map((m, i) => (
+                          <li key={i} className={c.listu}>
+                            {m}
+                          </li>
+                        ))}
+                      </ul>
+                    </td>
+                    <td>{m.details}</td>
+                    <td>{m.pDD}</td>
+                    <td style={stylec(m.cableStatus)}>{m.cableStatus}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          {isLoged.role === "ShiftLeader" && (
+            <React.Fragment>
+              <h2 className={c.titleCabsl}>scrap cables</h2>
+              <table
+                className={`${c.table} ${c.tableurscrap}`}
+                // style={{ marginBottom: "2rem" }}
+              >
+                <thead>
+                  <tr style={{ backgroundColor: "black" }}>
+                    <th width="auto">date/time</th>
+                    <th width="auto">Ref</th>
+                    <th>Equipe</th>
+                    <th width="15%">Problem</th>
+                    <th width="15%">Details</th>
+                    <th>pdd*</th>
+                    <th>Status</th>
+                    <th width="25%">Sl. Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {scrap.length > 0 &&
+                    scrap.map((m) => (
+                      <tr
+                        key={m._id}
+                        className={isLoged.role !== "Reworker" && c.hostin}
+                        onClick={(e) => trClicked(e, m)}
+                      >
+                        <td>
+                          {m.createdAt.split("T")[0]}
+                          <br />
+                          <span className={c.timed}>
+                            {m.createdAt.split("T")[1].split(":")[0]}:
+                            {m.createdAt.split("T")[1].split(":")[1]}
+                          </span>
+                        </td>
+                        <td>{m.reference}</td>
+                        <td>{m.crew}</td>
+                        <td style={{ padding: "8px 0" }}>
+                          <ul className={c.underl}>
+                            {m.problem.map((m, i) => (
+                              <li key={i} className={c.listu}>
+                                {m}
+                              </li>
+                            ))}
+                          </ul>
+                        </td>
+                        <td>{m.details}</td>
+                        <td>{m.pDD}</td>
+                        <td style={stylec(m.cableStatus)}>{m.cableStatus}</td>
+                        <td>{m.shiftLeaderAction}</td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </React.Fragment>
+          )}
+
+          {isLoged.role === "ShiftLeader" && (
+            <div className={c.pagination}>
+              <span
+                className={c.paginationd}
+                onClick={(e) => onclickHandler(e, "min", "scrap")}
+              >
+                &laquo;
+              </span>
+              <span>
+                {pagescrap.page}/{pagescrap.totalPage}
+              </span>
+              <span
+                className={c.paginationd}
+                onClick={(e) => onclickHandler(e, "plus", "scrap")}
+              >
+                &raquo;
+              </span>
+            </div>
+          )}
+
+          <h2 className={c.titleCab2}>all cables</h2>
+          <table className={`${c.table}`}>
             <thead>
               <tr style={{ backgroundColor: "black" }}>
                 <th width="auto">date/time</th>
@@ -220,12 +294,17 @@ const Home = (p) => {
                 <th width="15%">Details</th>
                 <th>pdd*</th>
                 <th>Status</th>
-                <th width="25%">Sl. Action</th>
+                <th>PPD*</th>
+                <th>IdPD*</th>
+                <th>CM. Action</th>
+                <th>CP*</th>
+                <th>audi. Action</th>
+                <th>Sl. Action</th>
               </tr>
             </thead>
             <tbody>
-              {scrap.length > 0 &&
-                scrap.map((m) => (
+              {data.length > 0 &&
+                data.map((m) => (
                   <tr
                     key={m._id}
                     className={isLoged.role !== "Reworker" && c.hostin}
@@ -253,127 +332,62 @@ const Home = (p) => {
                     <td>{m.details}</td>
                     <td>{m.pDD}</td>
                     <td style={stylec(m.cableStatus)}>{m.cableStatus}</td>
+                    <td>{m.pPD}</td>
+                    <td>{m.idPD}</td>
+                    <td>{m.teamLeaderAction}</td>
+                    <td>{m.cP}</td>
+                    <td>{m.auditorAction}</td>
                     <td>{m.shiftLeaderAction}</td>
                   </tr>
                 ))}
             </tbody>
           </table>
-        </React.Fragment>
-      )}
-
-      {isLoged.role === "ShiftLeader" && (
-        <div className={c.pagination}>
-          <span
-            className={c.paginationd}
-            onClick={(e) => onclickHandler(e, "min", "scrap")}
-          >
-            &laquo;
-          </span>
-          <span>
-            {pagescrap.page}/{pagescrap.totalPage}
-          </span>
-          <span
-            className={c.paginationd}
-            onClick={(e) => onclickHandler(e, "plus", "scrap")}
-          >
-            &raquo;
-          </span>
+          <div className={c.pagination}>
+            <span
+              className={c.paginationd}
+              onClick={(e) => onclickHandler(e, "min")}
+            >
+              &laquo;
+            </span>
+            <span>
+              {page.page}/{page.totalPage}
+            </span>
+            <span
+              className={c.paginationd}
+              onClick={(e) => onclickHandler(e, "plus")}
+            >
+              &raquo;
+            </span>
+          </div>
+          <div className={c.ter}>
+            <ul className={c.underList}>
+              <li className={c.list}>
+                PDD*: <span>poste détecte défaut </span>
+              </li>
+              <li className={c.list}>
+                PPD*: <span>poste produit défaut</span>
+              </li>
+              <li className={c.list}>
+                IDPD*: <span>ID produit défaut</span>
+              </li>
+              <li className={c.list}>
+                CP*: <span>cause de problem</span>
+              </li>
+            </ul>
+          </div>
         </div>
       )}
+      {isLoged.role === "Logistics" && (
+        <div className={c.holder}>
+          <div className={c.holderlog}>
+          
+          <div className={c.holderref}></div>
+          <div className={c.holderupload}></div>
 
-      <h2 className={c.titleCab2}>all cables</h2>
-      <table className={`${c.table}`}>
-        <thead>
-          <tr style={{ backgroundColor: "black" }}>
-            <th width="auto">date/time</th>
-            <th width="auto">Ref</th>
-            <th>Equipe</th>
-            <th width="15%">Problem</th>
-            <th width="15%">Details</th>
-            <th>pdd*</th>
-            <th>Status</th>
-            <th>PPD*</th>
-            <th>IdPD*</th>
-            <th>CM. Action</th>
-            <th>CP*</th>
-            <th>audi. Action</th>
-            <th>Sl. Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.length > 0 &&
-            data.map((m) => (
-              <tr
-                key={m._id}
-                className={isLoged.role !== "Reworker" && c.hostin}
-                onClick={(e) => trClicked(e, m)}
-              >
-                <td>
-                  {m.createdAt.split("T")[0]}
-                  <br />
-                  <span className={c.timed}>
-                    {m.createdAt.split("T")[1].split(":")[0]}:
-                    {m.createdAt.split("T")[1].split(":")[1]}
-                  </span>
-                </td>
-                <td>{m.reference}</td>
-                <td>{m.crew}</td>
-                <td style={{ padding: "8px 0" }}>
-                  <ul className={c.underl}>
-                    {m.problem.map((m, i) => (
-                      <li key={i} className={c.listu}>
-                        {m}
-                      </li>
-                    ))}
-                  </ul>
-                </td>
-                <td>{m.details}</td>
-                <td>{m.pDD}</td>
-                <td style={stylec(m.cableStatus)}>{m.cableStatus}</td>
-                <td>{m.pPD}</td>
-                <td>{m.idPD}</td>
-                <td>{m.teamLeaderAction}</td>
-                <td>{m.cP}</td>
-                <td>{m.auditorAction}</td>
-                <td>{m.shiftLeaderAction}</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-      <div className={c.pagination}>
-        <span
-          className={c.paginationd}
-          onClick={(e) => onclickHandler(e, "min")}
-        >
-          &laquo;
-        </span>
-        <span>
-          {page.page}/{page.totalPage}
-        </span>
-        <span
-          className={c.paginationd}
-          onClick={(e) => onclickHandler(e, "plus")}
-        >
-          &raquo;
-        </span>
-      </div>
-      <div className={c.ter}>
-        <ul className={c.underList}>
-          <li className={c.list}>
-            PDD*: <span>poste détecte défaut </span>
-          </li>
-          <li className={c.list}>
-            PPD*: <span>poste produit défaut</span>
-          </li>
-          <li className={c.list}>
-            IDPD*: <span>ID produit défaut</span>
-          </li>
-          <li className={c.list}>
-            CP*: <span>cause de problem</span>
-          </li>
-        </ul>
-      </div>
-    </div>
+          </div>
+        </div>
+      )}
+    </React.Fragment>
   );
 };
 
