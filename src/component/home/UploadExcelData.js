4589477fcdf0c2a17e83783d9c3c0c2052aae2a4
anass zeroual <anasszeroual09@gmail.com>
2024-02-16 16:22:43 +0100

    pushincki

--- src/component/home/UploadExcelData.js
+++ src/component/home/UploadExcelData.js
@@ -1,8 +1,8 @@
 import React, { useRef, useState } from "react";
 import c from "./UploadExcelData.module.css";
-
+import api from "../../services/api";
 import { useSelector } from "react-redux";
-// import Notification from "./Notification";
+import Notification from "./Notification";
 import UploadLoading from "../ui/UploadLoading";
 
 const UploadExcelData = (p) => {
@@ -40,31 +40,51 @@ const UploadExcelData = (p) => {
     setShowbutton(true);
   };
 
-  const handleButtonClick = () => {
+  const handleButtonClick = async () => {
     setLoading(true);
     setShowbutton(false);
 
-    // const jwtToken = login.token;
-    // const formData = new FormData();
-    // formData.append("file", file);
-    // fetch(`http://dl5wv9gj3.aptiv.com:8081/admin/uploadData`, {
+    // const jwtToken = isLoged.token;
+    const formData = new FormData();
+    formData.append("excel", file);
+    // fetch(`${api}/logistics`, {
     //   method: "POST",
     //   body: formData,
     //   headers: {
     //     Authorization: `Bearer ${jwtToken}`,
     //   },
-    // })
-    //   .then((response) => {
-    //     if (response.status === 200) {
-    //         setSuccessfully(true);
-    //         setLoading(false);
-    //     } else {
-    //         setError(true);
+    // }).then((response) => {
+    //   if (response.status === 200) {
+    //     setSuccessfully(true);
+    //     setLoading(false);
+    //   } else {
+    //     setError(true);
     //     setLoading(false);
     //     setShowbutton(true);
-    //       throw new Error(`Request failed with status: ${response.status}`);
-    //     }
-    //   })
+    //     throw new Error(`Request failed with status: ${response.status}`);
+    //   }
+    // });
+    try {
+      const response = await fetch(`${api}/logistics`, {
+        method: "POST",
+        body: formData,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${isLoged.token}`,
+        },
+      });
+
+      const data = await response.json();
+      console.log(data)
+      setSuccessfully(true);
+        setLoading(false);
+      // dispatch(loginSActions.addLogisticsData(data.reverse()));
+    } catch (error) {
+      setError(true);
+        setLoading(false);
+        setShowbutton(true);
+      console.error("Error:", error);
+    }
   };
 
   if (successfully) {
@@ -77,71 +97,73 @@ const UploadExcelData = (p) => {
       setError(false);
     }, 8000);
   }
-  // {successfully && <Notification file={true}/>}
-  // {error && <Notification error={true} />}
-//   <input
-//   type="file"
-//   id="images"
-//   ref={fileInputRef}
-//   accept=".xlsx"
-//   onChange={fileChangeHandler}
-//   required
-// />
+
+  //   <input
+  //   type="file"
+  //   id="images"
+  //   ref={fileInputRef}
+  //   accept=".xlsx"
+  //   onChange={fileChangeHandler}
+  //   required
+  // />
   return (
-    <div className={c.wrap}>
-      <label
-        htmlFor="images"
-        className={c["drop-container"]}
-        ref={dropContainerRef}
-        onDragOver={handleDragOver}
-        onDragEnter={handleDragEnter}
-        onDragLeave={handleDragLeave}
-        onDrop={handleDrop}
-        id="dropcontainer"
-      >
-        {loading ? (
-          <UploadLoading />
-        ) : (
-          <React.Fragment>
-            <span className={c["drop-title"]}>Drop excel file here</span>
-            or
-         
-            <div className={c["input-div"]}>
-              <input
-                className={c.input}
-                name="file"
-                type="file"
-                accept=".xlsx"
-                ref={fileInputRef}
-                onChange={fileChangeHandler}
-              />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="1em"
-                height="1em"
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                fill="none"
-                stroke="currentColor"
-                className={c.icon}
-              >
-                <polyline points="16 16 12 12 8 16" />
-                <line y2="21" x2="12" y1="12" x1="12" />
-                <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3" />
-                <polyline points="16 16 12 12 8 16" />
-              </svg>
-            </div>
-          </React.Fragment>
+    <React.Fragment>
+      {successfully && <Notification file={true} />}
+      {error && <Notification error={true} />}
+      <div className={c.wrap}>
+        <label
+          htmlFor="images"
+          className={c["drop-container"]}
+          ref={dropContainerRef}
+          onDragOver={handleDragOver}
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+          id="dropcontainer"
+        >
+          {loading ? (
+            <UploadLoading />
+          ) : (
+            <React.Fragment>
+              <span className={c["drop-title"]}>Drop excel file here</span>
+              or
+              <div className={c["input-div"]}>
+                <input
+                  className={c.input}
+                  name="file"
+                  type="file"
+                  accept=".xlsx"
+                  ref={fileInputRef}
+                  onChange={fileChangeHandler}
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                  className={c.icon}
+                >
+                  <polyline points="16 16 12 12 8 16" />
+                  <line y2="21" x2="12" y1="12" x1="12" />
+                  <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3" />
+                  <polyline points="16 16 12 12 8 16" />
+                </svg>
+              </div>
+            </React.Fragment>
+          )}
+        </label>
+        {showButton && (
+          <button className={c.button} onClick={handleButtonClick}>
+            Upload File
+          </button>
         )}
-      </label>
-      {showButton && (
-        <button className={c.button} onClick={handleButtonClick}>
-          Upload File
-        </button>
-      )}
-    </div>
+      </div>
+    </React.Fragment>
   );
 };
 
