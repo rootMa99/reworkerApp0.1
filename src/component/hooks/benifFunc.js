41ee6efea6cc504c9277a369aefb0b11f4f4143f
anass zeroual <anasszeroual09@gmail.com>
2024-03-26 08:36:03 +0000

    poutin

--- src/component/hooks/benifFunc.js
+++ src/component/hooks/benifFunc.js
@@ -1,13 +1,15 @@
 export const selectCreator = (data) => {
   const returnedArr = [];
-
-  data.length > 0 &&
-    data.map((m) =>
-      returnedArr.push({
-        value: m,
-        label: m,
-      })
-    );
+  console.log(data);
+  if (data !== undefined) {
+    data.length > 0 &&
+      data.map((m) =>
+        returnedArr.push({
+          value: m,
+          label: m,
+        })
+      );
+  }
 
   return returnedArr;
 };
