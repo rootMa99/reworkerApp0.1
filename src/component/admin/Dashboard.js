e8fbefd956f4333c351b5012dd884373f0f1b96e
anass zeroual <anasszeroual09@gmail.com>
2024-03-27 10:11:17 +0000

    fbi open the door

--- src/component/admin/Dashboard.js
+++ src/component/admin/Dashboard.js
@@ -1,35 +1,50 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import api from "../../services/api";
 
+const Dashboard = (p) => {
+  const { isLoged } = useSelector((s) => s.loginr);
+  const [crewsData, setCrewsData] = useState({});
+  const [crewsFetch, setCrewsFetch] = useState({
+    timeFrame: "monthly",
+    year: new Date().getFullYear(),
+    month: new Date().getMonth() + 1,
+    crews: "",
+    cableStatus: "",
+    problems: "",
+  });
+  const callback = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `${api}/data/chart/crew?timeFrame=${crewsFetch.timeFrame}&year=${crewsFetch.year}&month=${crewsFetch.month}&cableStatus=${crewsFetch.cableStatus}&crews=${crewsFetch.crews}&problems=${crewsFetch.problems}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${isLoged.token}`,
+          },
+        }
+      );
 
-const Dashboard=p=>{
-    const { isLoged } = useSelector((s) => s.loginr);
+      const data = await response.json();
+      console.log(data);
+      setCrewsData(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }, [
+    crewsFetch.timeFrame,
+    crewsFetch.year,
+    crewsFetch.month,
+    crewsFetch.cableStatus,
+    crewsFetch.crews,
+    crewsFetch.problems,
+    isLoged,
+  ]);
 
-    const callback=useCallback(async ()=>{
-        try {
-            const response = await fetch(`${api}/metadata`, {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${isLoged.token}`,
-              },
-            });
-      
-            const data = await response.json();
-            console.log(data);
-            dispatch(
-              loginSActions.addDataSelect(data)
-            );
-          } catch (error) {
-            console.error("Error:", error);
-          }
-    })
+  useEffect(() => callback(), [callback]);
 
-    return(
-        <React.Fragment>
-        </React.Fragment>
-    )
-}
+  return <React.Fragment></React.Fragment>;
+};
 
 export default Dashboard;
