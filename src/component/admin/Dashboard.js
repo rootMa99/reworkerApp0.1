e088b894b530d0ada94168d56bad411f4bee748f
anass zeroual <anasszeroual09@gmail.com>
2024-03-29 09:27:05 +0000

    TERMPO

--- src/component/admin/Dashboard.js
+++ src/component/admin/Dashboard.js
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 import c from "./Dashboard.module.css";
 import { loginSActions } from "../../store/loginSlice";
 import { selectCreator } from "../hooks/benifFunc";
+import api from "../../services/api";
+import { Link } from "react-router-dom";
 const customStyles = {
   control: (provided, state) => ({
     ...provided,
@@ -69,27 +71,28 @@ const customStyles = {
   }),
 };
 const statusC = [
-    {
-      value: "in-Progress",
-      label: "in-Progress",
-    },
-    {
-      value: "Repaired",
-      label: "Repaired",
-    },
-    {
-      value: "Scrap",
-      label: "Scrap",
-    },
-    {
-      value: "Sertissage",
-      label: "Sertissage",
-    },
-  ];
+  {
+    value: "in-Progress",
+    label: "in-Progress",
+  },
+  {
+    value: "Repaired",
+    label: "Repaired",
+  },
+  {
+    value: "Scrap",
+    label: "Scrap",
+  },
+  {
+    value: "Sertissage",
+    label: "Sertissage",
+  },
+];
 const Dashboard = (p) => {
   const { dataFilter, dataSelect } = useSelector((s) => s.loginr);
   const dispatch = useDispatch();
   console.log(dataSelect);
+
   return (
     <React.Fragment>
       <div className={c.headerD}>
@@ -103,9 +106,7 @@ const Dashboard = (p) => {
               styles={customStyles}
               placeholder="select Crews"
               onChange={(e) => {
-                dispatch(
-                  loginSActions.editcrew(e)
-                );
+                dispatch(loginSActions.editcrew(e));
               }}
               isMulti
             />
@@ -119,9 +120,7 @@ const Dashboard = (p) => {
               styles={customStyles}
               placeholder="select problems"
               onChange={(e) => {
-                dispatch(
-                  loginSActions.editproblems(e)
-                );
+                dispatch(loginSActions.editproblems(e));
               }}
               isMulti
             />
@@ -135,9 +134,7 @@ const Dashboard = (p) => {
               styles={customStyles}
               placeholder="select Cable Status"
               onChange={(e) => {
-                dispatch(
-                  loginSActions.editcableStatus(e)
-                );
+                dispatch(loginSActions.editcableStatus(e));
               }}
               isMulti
             />
@@ -165,6 +162,35 @@ const Dashboard = (p) => {
           </div>
         </div>
       </div>
+      <div className={c.btnhelperholder}>
+        <Link to={`${api}/data/export/repairdata/excel?year=${
+          dataFilter.year
+        }&month=${dataFilter.month}&cableStatus=${dataFilter.cableStatus
+          .map((m) => m.value)
+          .toString()}&crews=${dataFilter.crews
+          .map((m) => m.value)
+          .toString()}&problems=${dataFilter.problems
+          .map((m) => m.value)
+          .toString()}`}>
+          <button className={c.button} type="button">
+            <span className={c["button__text"]}>Download</span>
+            <span className={c["button__icon"]}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 35 35"
+                id="bdd05811-e15d-428c-bb53-8661459f9307"
+                data-name="Layer 2"
+                className={c.svg}
+              >
+                <path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
+                <path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
+                <path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
+              </svg>
+            </span>
+          </button>
+        </Link>
+      </div>
+
       <ReUtilChartDashbord title="crews" type="crew" />
       <ReUtilChartDashbord title="problems" type="problem" />
       <ReUtilChartDashbord title="cablestatuses" type="cableStatus" />
