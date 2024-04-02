3d2a1fe5265c591ba0354a9649822504298ea297
anass zeroual <anasszeroual09@gmail.com>
2024-04-02 10:55:15 +0000

    wit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 
-//  const api = 'http://10.236.148.13:8082/api';
- const api = 'http://10.236.150.30:88/api';
+ const api = 'http://10.236.148.13:8082/api';
+//  const api = 'http://10.236.150.30:88/api';
  export default api;
 
 
