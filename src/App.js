b7eaf4a441a22b420127368affe915ed70b9abfd
anass zeroual <anasszeroual09@gmail.com>
2024-04-03 09:32:18 +0000

    wh_te

--- src/App.js
+++ src/App.js
@@ -8,38 +8,12 @@ import Login from "./component/ui/Login";
 import Admin from "./component/admin/Admin";
 import Dashboard from "./component/admin/Dashboard";
 import FeedBackForm from "./component/home/FeedBackForm";
-// import api from "./services/api";
-// import { loginSActions } from "./store/loginSlice";
+
 
 function App() {
   const { isLoged } = useSelector((s) => s.loginr);
   const location = useLocation();
   const currentPath = location.pathname;
-  //   const dispatch=useDispatch();
-
-  //   const callback=useCallback(async ()=>{
-  //     try {
-  //       const response = await fetch(`${api}/metadata`, {
-  //         method: "GET",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           Authorization: `Bearer ${isLoged.token}`,
-  //         },
-  //       });
-
-  //       const data = await response.json();
-  //       console.log(data);
-  //       dispatch(
-  //         loginSActions.addDataSelect(data)
-  //       );
-  //     } catch (error) {
-  //       console.error("Error:", error);
-  //     }
-  //   }, [dispatch, isLoged.token])
-
-  // useEffect(()=>{
-  //   callback();
-  // }, [callback])
 
   return (
     <div className="App">
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,3 @@
-
- const api = 'http://10.236.148.13:8082/api';
+const api = "http://10.236.148.13:8082/api";
 //  const api = 'http://10.236.150.30:88/api';
- export default api;
-
-
+export default api;
